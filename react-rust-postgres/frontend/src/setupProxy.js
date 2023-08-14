8a76c9309c023369ffc875eac32a867cf31028b4
Ryan McLean <ryan1_00@hotmail.com>
2023-08-14 17:24:40 +0100

    Clarify which backend is for compose. Add master kustomization file

--- react-rust-postgres/frontend/src/setupProxy.js
+++ react-rust-postgres/frontend/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
   app.use(
     "/api",
     createProxyMiddleware({
+      //target: "http://backend:8000", // Docker Compose value
       target: "http://backend.default.svc.cluster.local:8080",
-      //target: "http://backend:8000",
       //target: "http://127.0.0.1:8000",
       //target: `http://${process.env.REACT_APP_BACKEND}`,
       //target: `http://${process.env.REACT_APP_BACKEND}`,
